cc63ab6cc35936159ab6e1e911676e2ee83065e1
unknown <>
2015-11-29 14:11:40 +0700

    create function

--- Captcha.php
+++ Captcha.php
@@ -6,6 +6,9 @@
 	{
 		private $pattern;
 		private $right_operand;
+		private $left_operand;
+		private $operator;
+		private $res;
 		public function setPattern($pattern){
 
 			$this->pattern=$pattern;
@@ -15,6 +18,10 @@
 			$this->right_operand = $number;
 
 		}
+		public function setLeftOperand($number){
+			$this->left_operand = $number;
+		}
+
 		public function getRightOperand(){
 			if ($this->pattern == 1) {
 				$result = array(0 => 'zero' ,
@@ -28,8 +35,45 @@
 								8 => 'eight' ,
 								9 => 'nine' ,
 								10 => 'ten' );
+				$this->right_operand=$result[$this->right_operand];
+				return $this->right_operand;
+			}else 
+				return $this->right_operand;
+		}
+		public function setOperator($op){
+			if($op==1)
+				$this->operator='+';
+			elseif($op==2)$this->operator='-';
+
+		}
+		public function getQuestion(){
+			if($this->pattern == 1 )
+				$this->res = $this->left_operand." ".$this->operator." ".$this->right_operand." = ?";
+			return $this->res;
+		}
+			
+		public function getLeftOperand(){
+			if ($this->pattern == 1) {
 				
-				return $result[$this->right_operand];
+				
+				return $this->left_operand;
+
+			}
+			else{
+				$result = array(0 => 'zero' ,
+								1 => 'one' ,
+								2 => 'two' ,
+								3 => 'three' ,
+								4 => 'four' ,
+								5 => 'five' ,
+								6 => 'six' ,
+								7 => 'seven' ,
+								8 => 'eight' ,
+								9 => 'nine' ,
+								10 => 'ten' );
+				
+				return $result[$this->left_operand];
+			
 			}
 		}
 	}
--- TestCaptcha.php
+++ TestCaptcha.php
@@ -1,14 +1,50 @@
 <?php 
-	/**
-	* 
+	/*
+	pattern 1
+	3+ three
+
+	pattern 2
+	six -  7
 	*/
 	class TestCaptcha extends PHPUnit_Framework_TestCase{
 		public function testSetRightOperandPattern1With5reternFive(){
 			$c = new Captcha();
 			$c->setPattern(1);
-			$c->setRightOperand(5);
-			$this->assertEquals('five',$c->getRightOperand());
+			$c->setRightOperand(1);
+			$this->assertEquals('one',$c->getRightOperand());
 		}
+		public function testLeftOperandPattern1With3Return3(){
+			$c = new Captcha();
+			$c->setPattern(1);
+			$c->setLeftOperand(1);
+			$this->assertEquals(1,$c->getLeftOperand());
+
+		}
+		public function testLeftOperandPattern2With6ReturnSix(){
+			$c = new Captcha();
+			$c->setPattern(2);
+			$c->setLeftOperand(6);
+			$this->assertEquals('six',$c->getLeftOperand());
+
+		}
+		public function testLeftOperandPattern2With7Return7(){
+			$c = new Captcha();
+			$c->setPattern(2);
+			$c->setRightOperand(7);
+			$this->assertEquals(7,$c->getRightOperand());
+
+		}
+		public function testQuestionPattern1(){
+			$c = new Captcha();
+			$c->setPattern(1);
+			$c->setLeftOperand(1);
+			$c->setOperator(1);
+			$c->setRightOperand(1);
+
+			$this->assertEquals("1 + one = ?",$c->getQuestion());
+
+		}
+
 
 
 	}
